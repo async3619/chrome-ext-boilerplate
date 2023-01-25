19575f04b9bf404f14cad7025d25e6508ea150be
Sophia <me@sophia-dev.io>
2023-01-26 04:10:17 +0900

    chore: update popup react app directory name

--- popup/App.tsx
+++ popup/App.tsx
@@ -21,7 +21,7 @@ function App() {
                     count is {count}
                 </button>
                 <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
+                    Edit <code>popup/App.tsx</code> and save to test HMR
                 </p>
             </div>
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
